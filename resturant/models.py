f8bce708765b227d504682699a64050321909dbd
MajedALRamdan <majooddark237514@gmail.com>
2022-07-20 18:12:35 +0300

    time edited

--- resturant/models.py
+++ resturant/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Restaurant(models.Model):
     name = models.CharField(max_length=30)
-    description = models.CharField(max_length=100,)
-    opening_time=models.TimeField()
-    closing_time=models.TimeField()
+    description = models.CharField(max_length=100,default="")
+    opening_time=models.TimeField(default="00:00:00")
+    closing_time=models.TimeField(default="00:00:00")
     created_at=models.DateTimeField(auto_now_add=True)
    
